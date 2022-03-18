16f729a404be22fae96467053ffafca543b6a912
ajpelu <ajpelu@gmail.com>
2022-03-18 12:04:24 +0100

    fix needed pkgs

--- scripts/01_getGPS_from_mail.R
+++ scripts/01_getGPS_from_mail.R
@@ -6,6 +6,7 @@
 
 # Get attachment from MAIL 
 library(mRpostman) # An IMAP Client for R, CRAN v1.0.0
+library(here) 
 
 # Set dir 
 setwd(here::here("rawdata/gps_mail")) 
@@ -24,6 +25,7 @@ con$select_folder(name = "INBOX")
 con$search_string(expr = "support@digitanimal.com", where = "FROM") %>% 
   con$fetch_body(write_to_disk = TRUE) %>% 
   con$get_attachments(override = TRUE)
+
 
 
 
