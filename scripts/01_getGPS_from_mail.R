ca00b286b408d4c78691c30f3731545e8b628747
ajpelu <ajpelu@gmail.com>
2022-05-03 10:04:14 +0200

    add temporal filter

--- scripts/01_getGPS_from_mail.R
+++ scripts/01_getGPS_from_mail.R
@@ -7,6 +7,7 @@
 # Get attachment from MAIL 
 library(mRpostman) # An IMAP Client for R, CRAN v1.0.0
 library(here) 
+library(tidyverse)
 
 # Set dir 
 setwd(here::here("rawdata/gps_mail")) 
@@ -21,10 +22,19 @@ con <- configure_imap(
 # Select INBOX 
 con$select_folder(name = "INBOX") 
 
+# Set the filter for date 
+# emails are received on the first month's day, so we search after the lasts 
+# days of the previous month
+f <- "29-Apr-2022"
+
 # Get attachment
-con$search_string(expr = "support@digitanimal.com", where = "FROM") %>% 
+con$search(request = AND(string(expr = "support@digitanimal.com", 
+                                where = "FROM"),
+                         since(date_char = f))) %>% 
   con$fetch_body(write_to_disk = TRUE) %>% 
   con$get_attachments(override = TRUE)
+
+
 
 
 
